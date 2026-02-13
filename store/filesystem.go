5b48fa6677cbff61eac5cc117c4deae45e525746
unknown <vuqarss@hotmail.com>
2026-02-13 18:39:59 +0100

    repackage&refactor

---
+++ store/filesystem.go
@@ -0,0 +1,76 @@
+package store
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type FileSystem interface {
+	Open(name string) (*os.File, error)
+	CreateTemp(dir string, pattern string) (*os.File, error)
+	Create(path string) (*os.File, error)
+	Remove(name string) error
+	ReadDir(name string) ([]fs.DirEntry, error)
+	acquireExclusiveLock(directory string) (*os.File, error)
+	acquireSharedLock(directory string) (*os.File, error)
+}
+
+type OSFileSystem struct{}
+
+func (fs OSFileSystem) acquireExclusiveLock(directory string) (*os.File, error) {
+	filePath := filepath.Join(directory, ".lock")
+
+	lockFile, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0644)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create lock file: %w", err)
+	}
+
+	if err := Lock(lockFile); err != nil {
+		lockFile.Close()
+		return nil, err
+	}
+
+	lockFile.Truncate(0)
+	lockFile.Seek(0, 0)
+	fmt.Fprintf(lockFile, "%d\n", os.Getpid())
+	lockFile.Sync()
+
+	return lockFile, nil
+}
+func (fs OSFileSystem) acquireSharedLock(directory string) (*os.File, error) {
+	filePath := filepath.Join(directory, ".lock")
+
+	lockFile, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0644)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create lock file: %w", err)
+	}
+
+	if err := LockShared(lockFile); err != nil {
+		lockFile.Close()
+		return nil, err
+	}
+	return lockFile, nil
+}
+
+func (OSFileSystem) CreateTemp(dir string, pattern string) (*os.File, error) {
+	return os.CreateTemp(dir, pattern)
+}
+func (OSFileSystem) Create(path string) (*os.File, error) {
+	return os.Create(path)
+}
+
+func (OSFileSystem) Open(name string) (*os.File, error) {
+	return os.Open(name)
+}
+
+func (OSFileSystem) ReadDir(name string) ([]fs.DirEntry, error) {
+	return os.ReadDir(name)
+}
+
+func (OSFileSystem) Remove(name string) error {
+	return os.Remove(name)
+}
